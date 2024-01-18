e6e19e94fe7cb554857227fb64af4c59e323ccf4
Andrew Glago <andrewglago1@gmail.com>
2024-01-18 20:27:07 +0000

    Dynamic path params

--- src/06-challenges/37-get-dynamic-path-params.problem.ts
+++ src/06-challenges/37-get-dynamic-path-params.problem.ts
@@ -4,7 +4,19 @@ type UserPath = "/users/:id";
 
 type UserOrganisationPath = "/users/:id/organisations/:organisationId";
 
-type ExtractPathParams = unknown;
+type ValidatePath<P> = P extends `:${string}` ? P : never;
+
+type SplitPathSegments<P> = P extends `${infer First}/${infer Rest}`
+  ? [ValidatePath<First>, ...SplitPathSegments<Rest>]
+  : [ValidatePath<P>];
+
+type ValidPaths<T> = SplitPathSegments<T>[number];
+
+type ToPathParams<P extends string> = {
+  [K in ValidPaths<P> as K extends `:${infer Key}` ? Key : never]: string;
+};
+
+type ExtractPathParams<T extends string> = ToPathParams<T>;
 
 type tests = [
   Expect<Equal<ExtractPathParams<UserPath>, { id: string }>>,
@@ -13,5 +25,5 @@ type tests = [
       ExtractPathParams<UserOrganisationPath>,
       { id: string; organisationId: string }
     >
-  >,
+  >
 ];
